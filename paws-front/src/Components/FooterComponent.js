07a8852522f07aee3a25792308d95ae3cfb83472
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-05-17 08:12:43 +0100

    Styling the footer

--- paws-front/src/Components/FooterComponent.js
+++ paws-front/src/Components/FooterComponent.js
@@ -1,41 +1,46 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { a } from "react-router-dom";
 
-export const Footer = () => {
-    <div className="footer-div">
-        <div className="brand">
-            
+function Footer() {
+    return (
+        <div className="footer-div">
+            <div className="brand">
+                <h3>Paws</h3>
+            </div>
+            <div className="contact">
+                <h4>
+                    Get in touch
+                </h4>
+                <h4>
+                    MON-FRI <br/>
+                    8AM - 5PM
+                </h4>
+                <li>
+                    <a href="https://intsgram.com">instagram</a>
+                </li>
+                <li>
+                    <a href="https://facebook.com">facebook</a>
+                </li>
+                <li>
+                    <a href="https://pinterest.com">pinterest</a>
+                </li>
+            </div>
+            <div className="subscribe">
+                <h3>
+                    join our mailing list
+                </h3>
+                <form action="submit">
+                    
+                </form>
+            </div>
+            <div className="explore">
+                <h3>Explore</h3>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+            </div>
         </div>
-        <div className="contact">
-            <h4>
-                Get in touch
-            </h4>
-            <h4>
-                MON-FRI
-                8AM - 5PM
-            </h4>
-            <li>
-                <a href="https://intsgram.com">INSTAGRAM</a>
-            </li>
-            <li>
-                <a href="https://facebook.com">FACEBOOK</a>
-            </li>
-            <li>
-                <a href="https://pinterest.com">PINTREST</a>
-            </li>
-        </div>
-        <div className="subscribe">
-            <h3>
-                join our mailing list
-            </h3>
-            <form action="submit">
-                
-            </form>
-        </div>
-        <div className="explore">
-            <Link to="/">Home</Link>
-            <Link>About</Link>
-            <Link>Contact</Link>
-        </div>
-    </div>
+    )
 }
+
+export default Footer;
